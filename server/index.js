f333d2cd1fca91b43da19f061ccb35ef070f1fab
mariamisa <maryaam1998@hotmail.com>
2020-06-17 15:58:16 +0300

    fix port relates #5

--- server/index.js
+++ server/index.js
@@ -4,5 +4,5 @@ const port = app.get('port');
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log('http://localhost:8000');
+  console.log(`http://localhost:${port}`);
 });
