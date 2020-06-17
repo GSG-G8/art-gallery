d8eddb3a4e9ab3a0e06cf850f68cce201c150cf1
mariamisa <maryaam1998@hotmail.com>
2020-06-17 15:41:35 +0300

    set-up express server relates #5

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+
+const app = express();
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+app.disabled('x-powered-by');
+app.use(cookieParser());
+app.use(express.json());
+app.use(compression());
+app.set('port', process.env.PORT || 5000);
+
+app.get('/', (req, res) => {
+  res.status(200).json({ msg: 'hi from server' });
+});
+
+module.exports = app;
