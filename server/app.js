f4e27ca5f577d0c4718261215cf71459ebcc55de
mariamisa <maryaam1998@hotmail.com>
2020-06-17 16:06:42 +0300

    update relates #5

--- server/app.js
+++ server/app.js
@@ -4,11 +4,19 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 
 const app = express();
-if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
 app.disabled('x-powered-by');
-app.use(cookieParser());
-app.use(express.json());
-app.use(compression());
+const middleware = [
+  compression(),
+  cookieParser(),
+  express.json(),
+  express.urlencoded({ extended: false }),
+];
+
+app.use(middleware);
+
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
 app.set('port', process.env.PORT || 5000);
 
 module.exports = app;
