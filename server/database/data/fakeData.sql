2435203575375f433805549120ec55abcce3401e
AlaaSaadeddin <alaa.swaireh@gmail.com>
2020-06-18 11:10:13 +0300

    make some edits relates #27

--- server/database/data/fakeData.sql
+++ server/database/data/fakeData.sql
@@ -1,5 +1,5 @@
-INSERT INTO artists (firstName,lastName,email,password,mobileNo,customized,
-reviews,profileImg,socialMediaAccounts,budget,bio) VALUES 
+INSERT INTO artist (first_name,last_name,email,password,mobile_no,customized,
+reviews,profile_img,social_media_accounts,budget,bio) VALUES 
 ('Alaa','Swaireh','alaa@gmail.com','12345678',
 '0592888888','false','10','https://avatars1.githubusercontent.com/u/26024288?s=96&v=4','https://www.facebook.com/',
 '10.2','الفن هو الأسلوب، والأسلوب هو الإنسان');
@@ -10,7 +10,7 @@ reviews,profileImg,socialMediaAccounts,budget,bio) VALUES
 '0592888888','false','7','https://avatars1.githubusercontent.com/u/26024288?s=96&v=4','https://www.pinterest.com/',
 '10.2','الفن هو الأسلوب، والأسلوب هو الإنسان');
 
-INSERT INTO customers (firstName,lastName,email,password,budget) VALUES
+INSERT INTO customer (first_name,last_name,email,password,budget) VALUES
 ('Lina','Obeid','lina@gmail.com','00000000','20.44');
 ('Muhammad','Abed','mu95@gmail.com','555555555','12.24');
 ('Ahmed','Safi','ahmed@gmail.com','66666666','11.31');
@@ -28,7 +28,12 @@ INSERT INTO cart (customer_id,painting_id,price) VALUES
 ('2','1','150');
 ('3','3','250');
 
-INSERT INTO painting_user (painting_id,artist_id,customer_id,selling_date) VALUES
-('1','1','2','18/6/2020');
-('2','2','1','10/6/2020');
-('3','3','3','1/6/2020');  
+INSERT INTO painting_user (painting_id,customer_id,selling_date) VALUES
+('1','2','18/6/2020');
+('2','1','10/6/2020');
+('3','3','1/6/2020');  
+
+INSERT INTO feedback (artist_id,customer_id,rate,details) VALUES 
+('1','2','5','عمل رائع جدا');
+('2','1','4','عمل رائع جدا');
+('3','3','5','عمل رائع جدا');
