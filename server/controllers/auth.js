676a26673ddc4efbf6748846b9aeac721d9508ce
Lina Ebeid <linaebe0@gmail.com>
2020-06-18 10:47:11 +0300

    validate schema

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,16 @@
-exports.login = (req, res) => {
-  console.log('hi login');
+const { loginSchema } = require('../utils/validation');
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password, role } = await loginSchema.validate(req.body, {
+      abortEarly: false,
+    });
+  } catch (err) {
+    if (err.errors) {
+      res.status(400).json({
+        status: 400,
+        message: err.errors,
+      });
+    }
+  }
 };
