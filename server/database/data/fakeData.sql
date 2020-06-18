81d34b0e105370dfe1fbf7baa92668fe5230bf05
Lina Ebeid <linaebe0@gmail.com>
2020-06-18 16:16:58 +0300

    fix password at fakedata

--- server/database/data/fakeData.sql
+++ server/database/data/fakeData.sql
@@ -3,34 +3,25 @@ reviews,profile_img,social_media_accounts,budget,bio) VALUES
 ('Alaa','Swaireh','alaa@gmail.com','$2b$10$DcDZvSOFT8wnvO3uA89LGOTqa9rSRiOuaj2f7RxssF9DNwXYaeFXK',
 '0592888888','false','10','https://avatars1.githubusercontent.com/u/26024288?s=96&v=4','{https://www.facebook.com/}',
 '200.26','الفن هو الأسلوب، والأسلوب هو الإنسان'),
-('mariam','Isa','mariam@gmail.com','$2b$10$DcDZvSOFT8wnvO3uA89LGOTqa9rSRiOuaj2f7RxssF9DNwXYaeFXK
-',
+('mariam','Isa','mariam@gmail.com','$2b$10$DcDZvSOFT8wnvO3uA89LGOTqa9rSRiOuaj2f7RxssF9DNwXYaeFXK',
 '0592000000','true','12','https://avatars1.githubusercontent.com/u/26024288?s=96&v=4','{https://www.instagram.com/}',
 '300.50','الفن هو الأسلوب، والأسلوب هو الإنسان'),
-('Rana','Obeid','rana@gmail.com','$2b$10$DcDZvSOFT8wnvO3uA89LGOTqa9rSRiOuaj2f7RxssF9DNwXYaeFXK
-',
+('Rana','Obeid','rana@gmail.com','$2b$10$DcDZvSOFT8wnvO3uA89LGOTqa9rSRiOuaj2f7RxssF9DNwXYaeFXK',
 '0592111111','false','7','https://avatars1.githubusercontent.com/u/26024288?s=96&v=4','{https://www.pinterest.com/}',
 '300.66','الفن هو الأسلوب، والأسلوب هو الإنسان'),
-('Rehab','ALsawaf','rehab@gmail.com','$2b$10$DcDZvSOFT8wnvO3uA89LGOTqa9rSRiOuaj2f7RxssF9DNwXYaeFXK
-',
+('Rehab','ALsawaf','rehab@gmail.com','$2b$10$DcDZvSOFT8wnvO3uA89LGOTqa9rSRiOuaj2f7RxssF9DNwXYaeFXK',
 '0592333333','false','8','https://avatars1.githubusercontent.com/u/26024288?s=96&v=4','{https://www.facebook.com/}',
 '400.20','الفن هو الأسلوب، والأسلوب هو الإنسان'),
-('Mohammed','ALghzali','mohammed@gmail.com','$2b$10$DcDZvSOFT8wnvO3uA89LGOTqa9rSRiOuaj2f7RxssF9DNwXYaeFXK
-',
+('Mohammed','ALghzali','mohammed@gmail.com','$2b$10$DcDZvSOFT8wnvO3uA89LGOTqa9rSRiOuaj2f7RxssF9DNwXYaeFXK',
 '0592777777','true','3','https://avatars1.githubusercontent.com/u/26024288?s=96&v=4','{https://www.facebook.com/}',
 '100.23','الفن هو الأسلوب، والأسلوب هو الإنسان');
 
 INSERT INTO customer (first_name,last_name,email,password,budget) VALUES
-('Lina','Ebeid','lina@gmail.com','$2b$10$DcDZvSOFT8wnvO3uA89LGOTqa9rSRiOuaj2f7RxssF9DNwXYaeFXK
-','300.44'),
-('Muhammad','Abed','mu95@gmail.com','$2b$10$DcDZvSOFT8wnvO3uA89LGOTqa9rSRiOuaj2f7RxssF9DNwXYaeFXK
-','200.24'),
-('Ahmed','Safi','ahmed@gmail.com','$2b$10$DcDZvSOFT8wnvO3uA89LGOTqa9rSRiOuaj2f7RxssF9DNwXYaeFXK
-','200.31'),
-('Rawan','Abudahrouj','rwan@gmail.com','$2b$10$DcDZvSOFT8wnvO3uA89LGOTqa9rSRiOuaj2f7RxssF9DNwXYaeFXK
-','600.44'),
-('Abeer','Karam','abeer@gmail.com','$2b$10$DcDZvSOFT8wnvO3uA89LGOTqa9rSRiOuaj2f7RxssF9DNwXYaeFXK
-','1000.64');
+('Lina','Ebeid','lina@gmail.com','$2b$10$DcDZvSOFT8wnvO3uA89LGOTqa9rSRiOuaj2f7RxssF9DNwXYaeFXK','300.44'),
+('Muhammad','Abed','mu95@gmail.com','$2b$10$DcDZvSOFT8wnvO3uA89LGOTqa9rSRiOuaj2f7RxssF9DNwXYaeFXK','200.24'),
+('Ahmed','Safi','ahmed@gmail.com','$2b$10$DcDZvSOFT8wnvO3uA89LGOTqa9rSRiOuaj2f7RxssF9DNwXYaeFXK','200.31'),
+('Rawan','Abudahrouj','rwan@gmail.com','$2b$10$DcDZvSOFT8wnvO3uA89LGOTqa9rSRiOuaj2f7RxssF9DNwXYaeFXK','600.44'),
+('Abeer','Karam','abeer@gmail.com','$2b$10$DcDZvSOFT8wnvO3uA89LGOTqa9rSRiOuaj2f7RxssF9DNwXYaeFXK','1000.64');
 
 INSERT INTO painting (title,img,description,category,property,count_sold,artist_id) VALUES
 ('طائر الاوز','https://media.zid.store/0651e4a4-a220-4670-8922-c62f64ff8293/c69cd11f-6321-46b6-91fc-d4ea9769fbdf.jpeg',
