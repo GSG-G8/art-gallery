7e414711ce79e1a888611d303850828e931bbec2
Mu7ammadAbed <Mu7ammadAbed@gmail.com>
2020-06-18 16:49:27 +0300

    Change DB query style into text and value

--- server/database/queries/Paintings/getArtistPaintsQuery.js
+++ server/database/queries/Paintings/getArtistPaintsQuery.js
@@ -1,6 +1,9 @@
 const connection = require('../../connection');
 
 const getArtistPaintsQuery = (artistId) =>
-  connection.query('SELECT * FROM painting WHERE artist_id = $1', [artistId]);
+  connection.query({
+    text: 'SELECT * FROM painting WHERE artist_id = $1',
+    values: [artistId],
+  });
 
 module.exports = getArtistPaintsQuery;
