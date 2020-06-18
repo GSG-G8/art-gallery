f8850466097031c1c21733741db9d428fcc75569
Mu7ammadAbed <Mu7ammadAbed@gmail.com>
2020-06-18 16:27:20 +0300

    Add tests

--- server/controllers/getArtistPaints.js
+++ server/controllers/getArtistPaints.js
@@ -11,12 +11,12 @@ const getArtistPaints = async (req, res, next) => {
       } else {
         res.status(404).json({
           statusCode: 404,
-          message: "Sorry There's no cohort for this id",
+          message: "Sorry There's no artist for this id",
         });
       }
     } else {
-      res.status(404).json({
-        statusCode: 404,
+      res.status(400).json({
+        statusCode: 400,
         message: 'You enterd wrong artist ID',
       });
     }
--- test/paintings.test.js
+++ test/paintings.test.js
@@ -21,3 +21,46 @@ describe('Get all Paintings', () => {
       });
   });
 });
+
+describe('Get artist paints by id', () => {
+  test('Route /paintings/1 status 200, json header, data.name = ca-wiki ', (done) => {
+    return request(app)
+      .get('/api/v1/paintings/1')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        const {
+          data: { title },
+        } = res.body;
+        expect(title).toBe('طائر الاوز');
+        done();
+      });
+  });
+
+  test('Route /projects/18 status 200, json header ', (done) => {
+    return request(app)
+      .get('/api/v1/paintings/18')
+      .expect(404)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        const { message } = res.body;
+        expect(message).toBe("Sorry There's no artist for this id");
+        done();
+      });
+  });
+
+  test('Route /projects/gg status 404, json header ', (done) => {
+    return request(app)
+      .get('/api/v1/paintings/gg')
+      .expect(400)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        const { message } = res.body;
+        expect(message).toBe('You enterd wrong artist ID');
+        done();
+      });
+  });
+});
