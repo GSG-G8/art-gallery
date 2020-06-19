0becc52aeb9cdfac28c28493d145f1ec4199fee2
Mu7ammadAbed <Mu7ammadAbed@gmail.com>
2020-06-20 02:54:51 +0300

    Add DB query

--- server/database/queries/Cart/postCartQuery.js
+++ server/database/queries/Cart/postCartQuery.js
@@ -1,11 +1,12 @@
 const connection = require('../../connection');
 
 const postCartQuery = (reqData) => {
-  const { name, description, imgUrl, githubLink } = reqData;
-  return connection.query(
-    'INSERT INTO cohort (name, description, img_url, github_link) VALUES ($1, $2, $3, $4) RETURNING *;',
-    [name, description, imgUrl, githubLink],
-  );
+  const { customerId, paintingId } = reqData;
+  return connection.query({
+    text:
+      'INSERT INTO cart (customer_id, painting_id) VALUES ($1, $2) RETURNING *;',
+    values: [customerId, paintingId],
+  });
 };
 
 module.exports = postCartQuery;
