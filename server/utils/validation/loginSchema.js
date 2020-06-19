d86e7fff1525ca3053152a36bc29993b5e1b4923
Lina Ebeid <linaebe0@gmail.com>
2020-06-19 18:59:46 +0300

    specify role at login schema

--- server/utils/validation/loginSchema.js
+++ server/utils/validation/loginSchema.js
@@ -3,7 +3,7 @@ const yup = require('yup');
 const loginSchema = yup.object({
   email: yup.string().email().required(),
   password: yup.string().min(8).required(),
-  role: yup.string().required(),
+  role: yup.string().oneOf(['artist', 'user']).required(),
 });
 
 module.exports = loginSchema;
