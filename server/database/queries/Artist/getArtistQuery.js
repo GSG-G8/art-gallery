81428e536890f4fc11555828e33c0a2cb3e15c4a
AlaaSaadeddin <alaa.swaireh@gmail.com>
2020-06-19 18:29:08 +0300

    edit getArtistQuery relates #21

--- server/database/queries/Artist/getArtistQuery.js
+++ server/database/queries/Artist/getArtistQuery.js
@@ -1,6 +1,9 @@
 const connection = require('../../connection');
 
 const getArtistQuery = (id) =>
-  connection.query('SELECT * FROM artist WHERE id = $1', [id]);
+  connection.query({
+    text: 'SELECT * FROM artist WHERE id = $1',
+    values: [id],
+  });
 
 module.exports = getArtistQuery;
