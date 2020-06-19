af8c7d8d7420c19fd025dc364eadf6ce0a66100a
mariamisa <maryaam1998@hotmail.com>
2020-06-19 20:16:32 +0300

    solve conflict and delete dupplicated file

--- server/database/queries/artists/getArtist.js
+++
@@ -1,9 +0,0 @@
-const connection = require('../../connection');
-
-const getArtistByEmail = (email) =>
-  connection.query({
-    text: `SELECT * FROM artist WHERE email=$1;`,
-    values: [email],
-  });
-
-module.exports = getArtistByEmail;
