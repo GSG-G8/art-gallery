1aef32e1d097d73d002540fb0faee086816b0cfa
Mu7ammadAbed <Mu7ammadAbed@gmail.com>
2020-06-20 03:00:15 +0300

    Fix Validation

--- server/utils/validation/cartSchema.js
+++ server/utils/validation/cartSchema.js
@@ -1,8 +1,8 @@
 const yup = require('yup');
 
 const cartSchema = yup.object({
-  customer_id: yup.number().positive().required(),
-  painting_id: yup.number().positive().required(),
+  customerId: yup.number().positive().required(),
+  paintingId: yup.number().positive().required(),
 });
 
 module.exports = cartSchema;
