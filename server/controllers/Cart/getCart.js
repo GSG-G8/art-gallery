c27a105e8df8f9f894da41d18051b87aea58fa7e
Lina Ebeid <linaebe0@gmail.com>
2020-06-22 11:06:47 +0300

    getCart controller created

--- server/controllers/Cart/getCart.js
+++ server/controllers/Cart/getCart.js
@@ -1,4 +1,18 @@
+const { getCustomerCarts } = require('../../database/queries');
+
 exports.getCart = async (req, res, next) => {
-  const { id } = req.user;
-  console.log('hi id', id);
+  try {
+    const { id } = req.user;
+    const { rows } = await getCustomerCarts(id);
+    if (rows.length > 0) {
+      res.json({ statusCode: 200, data: rows });
+    } else {
+      res.status(200).json({
+        statusCode: 200,
+        message: "You don't have products at you cart",
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
 };
