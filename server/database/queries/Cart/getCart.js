43cf4780f4ffcd0b86956f79e7defccbba8a957b
Lina Ebeid <linaebe0@gmail.com>
2020-06-23 11:15:30 +0300

    edit getcart query to return painting columns

--- server/database/queries/Cart/getCart.js
+++ server/database/queries/Cart/getCart.js
@@ -2,7 +2,8 @@ const connection = require('../../connection');
 
 const getCustomerCarts = (customerID) =>
   connection.query({
-    text: 'SELECT * FROM cart WHERE customer_id = $1',
+    text:
+      'SELECT cart.id,cart.painting_id, painting.title, painting.img, painting.description, painting.category, painting.property, painting.artist_id FROM cart INNER JOIN painting ON cart.painting_id = painting.id WHERE cart.customer_id = $1',
     values: [customerID],
   });
 
