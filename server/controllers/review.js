edeba13b37e9bcdc836f432583c6c3394264db5a
Lina Ebeid <linaebe0@gmail.com>
2020-06-23 16:47:15 +0300

    create addReview controller

--- server/controllers/review.js
+++ server/controllers/review.js
@@ -1,14 +1,35 @@
 const { addReviewSchema } = require('../utils/validation');
+const { addReview } = require('../database/queries');
 
 exports.addReview = async (req, res, next) => {
   try {
+    const { id } = req.user;
     const { details, rate, artistID } = req.body;
-    const result = await addReviewSchema.validate(
-      { details, rate, artistID },
-      { abortEarly: false },
-    );
-    console.log('hi ', result);
+    if (Number.isInteger(rate)) {
+      await addReviewSchema.validate(
+        { details, rate, artistID },
+        { abortEarly: false },
+      );
+
+      const { rows } = await addReview(artistID, id, rate, details);
+      res.status(201).json({
+        StatusCode: 201,
+        data: { rows },
+        message: 'review added successfully',
+      });
+    } else {
+      res
+        .status(400)
+        .json({ statusCode: 400, message: 'rate should be integer' });
+    }
   } catch (err) {
-    next(err);
+    if (err.errors) {
+      res.status(400).json({
+        status: 400,
+        message: err.errors,
+      });
+    } else {
+      next(err);
+    }
   }
 };
--- server/routes/review.js
+++ server/routes/review.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
 const { addReview } = require('../controllers');
+const { verifyUser } = require('../controllers/middleware');
 
-router.post('/review', addReview);
+router.post('/review', verifyUser, addReview);
 
 module.exports = router;
