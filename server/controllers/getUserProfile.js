10b3051bd4f5a7dbdeaab2b8a850b8c40a883cc5
Lina Ebeid <linaebe0@gmail.com>
2020-06-23 11:34:19 +0300

    handle getProfile controller

--- server/controllers/getUserProfile.js
+++ server/controllers/getUserProfile.js
@@ -1,1 +1,11 @@
-exports.getUserProfile = async (req, res, next) => {};
+const { getCustomerProfile } = require('../database/queries');
+
+exports.getUserProfile = async (req, res, next) => {
+  try {
+    const { id } = req.user;
+    const { rows: data } = await getCustomerProfile(id);
+    res.json({ statusCode: 200, data });
+  } catch (err) {
+    next(err);
+  }
+};
