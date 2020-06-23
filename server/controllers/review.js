341344048c745f4a1f60b684a14e2709579e2ecf
Lina Ebeid <linaebe0@gmail.com>
2020-06-23 14:12:54 +0300

    create getArtist controller

--- server/controllers/review.js
+++ server/controllers/review.js
@@ -1,6 +1,21 @@
+const { getArtistReviews } = require('../database/queries');
+
 exports.getArtistReview = async (req, res, next) => {
   try {
     const { artistID } = req.params;
+    // eslint-disable-next-line no-restricted-globals
+    if (!isNaN(artistID)) {
+      const { rows: data } = await getArtistReviews(artistID);
+      if (data.length > 0) {
+        res.json({ statusCode: 200, data });
+      } else {
+        res.json({ statusCode: 200, data: [] });
+      }
+    } else {
+      res
+        .status(400)
+        .json({ statusCode: 400, message: 'Artist ID should be number' });
+    }
   } catch (err) {
     next(err);
   }
