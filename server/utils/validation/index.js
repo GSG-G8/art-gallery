0c4e642fdf6f45ab31df35a7ee47f1e7c03521b9
Lina Ebeid <linaebe0@gmail.com>
2020-06-23 16:00:35 +0300

    validation schema created for addReview

--- server/utils/validation/index.js
+++ server/utils/validation/index.js
@@ -1,9 +1,11 @@
 const registerSchema = require('./registerSchema');
 const loginSchema = require('./loginSchema');
 const addPaintingSchema = require('./addPaintingSchema');
+const addReviewSchema = require('./addReviewSchema');
 
 module.exports = {
   loginSchema,
   addPaintingSchema,
   registerSchema,
+  addReviewSchema,
 };
