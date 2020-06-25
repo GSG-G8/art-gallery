ec110cfb5131d655a8f3e35812061d85b64de48d
Lina Ebeid <linaebe0@gmail.com>
2020-06-25 12:11:29 +0300

    edit query add painting to return customized from artist

--- server/database/queries/Paintings/getPaintingCategory.js
+++ server/database/queries/Paintings/getPaintingCategory.js
@@ -2,7 +2,8 @@ const connection = require('../../connection');
 
 const getPaintingsQuery = (category) =>
   connection.query({
-    text: 'SELECT * FROM painting WHERE category=$1',
+    text:
+      'SELECT P.id, P.title,P.img,P.description,P.category,P.property,P.count_sold,P.artist_id, A.customized FROM painting P INNER JOIN artist A ON P.artist_id = A.id WHERE P.category=$1',
     values: [category],
   });
 module.exports = getPaintingsQuery;
