5b440d90060abd36708191c30cbef601060a3972
Lina Ebeid <linaebe0@gmail.com>
2020-06-28 19:31:57 +0300

    add card component to display paintings

--- client/src/components/PaintingSection/index.js
+++ client/src/components/PaintingSection/index.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import propTypes from 'prop-types';
+import { Card } from 'antd';
 
-function PaintingsSection() {
+function PaintingsSection({ paintings }) {
+  const [hovered, setHovered] = useState(false);
   return (
     <>
-      <h1>hiiiiii</h1>
+      {paintings && (
+        <div className="container">
+          {paintings.map((painting) => (
+            <>
+              <Card
+                // loading
+                className="card"
+                hoverable
+                style={{ width: 300 }}
+                cover={
+                  <img
+                    className="card__img"
+                    alt={painting.title}
+                    src={`https://res.cloudinary.com/dacf3uopo/image/upload/v1593353472/${painting.img}`}
+                    onMouseEnter={() => setHovered(true)}
+                    onMouseLeave={() => setHovered(false)}
+                  />
+                }
+              >
+                <Card.Meta
+                  title={painting.title}
+                  description={painting.description}
+                />
+              </Card>
+            </>
+          ))}
+        </div>
+      )}
     </>
   );
 }
-
+PaintingsSection.propTypes = {
+  paintings: propTypes.arrayOf(propTypes.object).isRequired,
+};
 export default PaintingsSection;
