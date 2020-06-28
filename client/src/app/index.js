e4c74d88271c92166ba8e6892a6ff24bc4320976
mariamisa <maryaam1998@hotmail.com>
2020-06-28 17:20:48 +0300

    add route to app

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Art Gallery websites</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/" />
+        </Switch>
+      </Router>
     </div>
   );
 }
