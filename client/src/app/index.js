663e4a188abfa9d521aa8bae5892f5135113e416
mariamisa <maryaam1998@hotmail.com>
2020-06-29 10:30:48 +0300

    custom theme

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-import * as ROUTES from '../Routes';
+import * as ROUTES from '../constants/routes';
 
 function App() {
   return (
