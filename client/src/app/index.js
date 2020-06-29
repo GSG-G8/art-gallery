b0f1a4c69f8495c223e9cb7ebf05afed2cd1df70
AlaaSaadeddin <alaa.swaireh@gmail.com>
2020-06-29 16:10:13 +0300

    edit the navbar and its style

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -6,8 +6,6 @@ import * as ROUTES from '../constants/routes';
 function App() {
   return (
     <div className="App">
-      <NavbarComponent />
-      <h1>Welcome to Art Gallery websites</h1>
       <Router>
         <Switch>
           <Route exact path={ROUTES.HOME_PAGE} component={NavbarComponent} />
--- client/src/components/common/Navbar/index.js
+++ client/src/components/common/Navbar/index.js
@@ -1,30 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Button } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import * as ROUTES from '../../../constants/routes';
+import navLogo from '../../../assets/images/berwaz.png';
 import './style.css';
 
 const NavbarComponent = () => {
   return (
-    <div className="navbar-container">
-      {/* <div className="nav-right">
-        <h3>برواز</h3>
-      </div> */}
-      <div className="nav-left">
-        <a to="/cart">
-          <FontAwesomeIcon
-            className="cart-icon"
-            icon={faShoppingCart}
-            color="#00695C"
-            size="lg"
-          />
-        </a>
-        <a to="/signup" className="nav-link">
-          مستخدم جديد
-        </a>
-        <Button className="nav-login-btn">تسجيل الدخول</Button>
+    <div className="main-navbar-container">
+      <div className="navbar-container">
+        <div className="nav-left">
+          <Link to={ROUTES.CART_PAGE}>
+            <FontAwesomeIcon
+              className="cart-icon"
+              icon={faShoppingCart}
+              size="lg"
+            />
+          </Link>
+          <Link to={ROUTES.SIGNUP_PAGE} className="nav-link">
+            مستخدم جديد
+          </Link>
+          <Link to={ROUTES.LOGIN_PAGE}>
+            <Button className="nav-login-btn">تسجيل الدخول</Button>
+          </Link>
+        </div>
+        <div className="nav-right">
+          <Link to={ROUTES.HOME_PAGE}>
+            <img className="nav-logo" src={navLogo} alt="logo" />
+          </Link>
+        </div>
       </div>
-      <div className="nav-line" />
     </div>
   );
 };
