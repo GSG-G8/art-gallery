590fab4111a57565ba6c0c6b7608fd83a1e34169
mariamisa <maryaam1998@hotmail.com>
2020-06-30 11:23:59 +0300

    add backgroung img to login page

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -27,71 +27,74 @@ const Login = (props) => {
       if (err.response) {
         e = err.response.data.message;
       } else {
-        e = 'حصل خطأ غير متوقع حاول مجددا مرة اخرى';
+        e = 'حصل خطأ غير متوقع حاول مجددًا مرةً أخرى';
       }
       setError(e);
       setLoaded(false);
     }
   };
   return (
-    <div className="form-container">
-      <h2 className="form-title">اهلا بك في موقع برواز</h2>
-      <h3 className="sub-title">سجل دخولك كـ</h3>
-      <Form name="normal_login" className="login-form" onFinish={onFinish}>
-        <Radio.Group
-          onChange={({ target: { value } }) => setRole(value)}
-          defaultValue="customer"
-          className="radio-g"
-        >
-          <Radio.Button className="radio-btn" value="customer">
-            مشتري
-          </Radio.Button>
-          <Radio.Button className="radio-btn" value="artist">
-            فنان
-          </Radio.Button>
-        </Radio.Group>
-        <Form.Item
-          name="email"
-          rules={[
-            { required: true, message: '!رجاء قم بادخال بريدك الالكتروني' },
-          ]}
-        >
-          <Input
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="الايميل"
-            className="form-input"
-          />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: '!رجاء قم بادخال كلمة المرور' }]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="كلمة المرور"
-            className="form-input"
-          />
-        </Form.Item>
-        <a className="login-form-forgot" href="/">
-          نسيت كلمة المرور؟
-        </a>
-
-        <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="login-form-button"
+    <div className="main-form-container">
+      <div className="form-container">
+        <h2 className="form-title">أهلًا بكَ في موقع برواز</h2>
+        <h3 className="sub-title">تسجيل الدخول كـ</h3>
+        <Form name="normal_login" className="login-form" onFinish={onFinish}>
+          <Radio.Group
+            onChange={({ target: { value } }) => setRole(value)}
+            defaultValue="customer"
+            className="radio-g"
           >
-            {loaded ? <Spin /> : 'تسجيل الدخول'}
-          </Button>
-          <br />
-          <a className="sign-up-btn" href="/login">
-            مستخدم جديد
-          </a>
-        </Form.Item>
-        {error && <Alert message={error} type="error" />}
-      </Form>
+            <Radio.Button className="radio-btn" value="customer">
+              مشترٍ
+            </Radio.Button>
+            <Radio.Button className="radio-btn" value="artist">
+              فنان
+            </Radio.Button>
+          </Radio.Group>
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: 'رجاءً قم بإدخال البريد الإلكتروني !',
+              },
+            ]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="الإيميل"
+              className="form-input"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              { required: true, message: 'رجاءً قم بإدخال كلمة المرور !' },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="كلمة المرور"
+              className="form-input"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              {loaded ? <Spin /> : 'تسجيل الدخول'}
+            </Button>
+            <br />
+            <a className="sign-up-btn" href="/signup">
+              مستخدم جديد
+            </a>
+          </Form.Item>
+          {error && <Alert message={error} type="error" />}
+        </Form>
+      </div>
     </div>
   );
 };
