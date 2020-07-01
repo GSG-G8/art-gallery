b6b37881c71f10c4900a0a448bddbc8e12a4e613
AlaaSaadeddin <alaa.swaireh@gmail.com>
2020-07-01 15:02:38 +0300

    create not found page

---
+++ client/src/components/PageNotFound/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Button } from 'antd';
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
+import notFoundImg from '../../assets/images/notFoundImg.png';
+import './style.css';
+
+const PageNotFound = () => {
+  return (
+    <div className="not-found-container">
+      <img src={notFoundImg} alt="notFound" />
+      <div>
+        <h1>عذراََ</h1>
+        <p>هذه الصفحة غير متوفرة</p>
+        <Link to={ROUTES.HOME_PAGE}>
+          <Button>الصفحة الرئيسية</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
