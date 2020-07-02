997bf10de8c49f6aabd0cc211f3a6089f1ec33db
Lina Ebeid <linaebe0@gmail.com>
2020-07-02 11:56:15 +0300

    inner join query to return last and first name

--- server/controllers/paintings/getPaintingInfo.js
+++ server/controllers/paintings/getPaintingInfo.js
@@ -5,8 +5,18 @@ const getPaintingInfo = async (req, res, next) => {
     params: { paintingId },
   } = req;
   try {
-    const { rows } = await getPaintQuery(paintingId);
-    res.status(200).json({ statusCode: 200, data: rows[0] });
+    if (paintingId > 0) {
+      const { rows } = await getPaintQuery(paintingId);
+      if (rows[0]) {
+        res.status(200).json({ statusCode: 200, data: rows[0] });
+      } else {
+        res
+          .status(400)
+          .json({ statusCode: 400, message: 'no painting for this id' });
+      }
+    } else {
+      res.status(400).json({ statusCode: 400, message: 'invalid id' });
+    }
   } catch (error) {
     next(error);
   }
--- server/database/queries/Paintings/getPaintingInfoQ.js
+++ server/database/queries/Paintings/getPaintingInfoQ.js
@@ -2,7 +2,8 @@ const connection = require('../../connection');
 
 const getPaintingQuery = (id) =>
   connection.query({
-    text: 'SELECT * FROM painting WHERE id = $1',
+    text:
+      'SELECT P.id, P.title,P.img,P.description,P.category,P.property,P.count_sold,P.artist_id, A.customized , A.first_name, A.last_name FROM painting P INNER JOIN artist A ON P.artist_id = A.id WHERE P.id=$1',
     values: [id],
   });
 
