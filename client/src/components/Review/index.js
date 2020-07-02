2bf88672ca48509aebf874011875440f28aba37f
AlaaSaadeddin <alaa.swaireh@gmail.com>
2020-07-02 12:54:39 +0300

    remove comment lines

--- client/src/components/Review/index.js
+++ client/src/components/Review/index.js
@@ -87,26 +87,3 @@ const ReviewPage = () => {
 };
 
 export default ReviewPage;
-
-// <div className="page-container">
-//   <div className="review-img-container">
-//     <img className="review-img" src={reviewPhoto} alt="reviewPhoto" />
-//   </div>
-//   <div className="review-container">
-//     <div className="rate-container">
-//       <h2 className="title-1">ما رأيك بأداء هذا الرسام ؟</h2>
-//       <h3 className="title-2">رأيك مهم بالنسبة لنا</h3>
-//       <Rate className="star-rate" defaultValue={0} count={5} onChange={(num)=>} />
-//       <div className="textarea-container">
-//         <h2 className="title-3">تعليق</h2>
-//         <textarea
-//           className="textarea-review"
-//           placeholder="رجاءاََ كن صادقاََ في تعليقك"
-//           rows="5"
-//           cols="50"
-//         />
-//         <Button className="review-btn" onClick={} >ارسال التعليق</Button>
-//       </div>
-//     </div>
-//   </div>
-// </div>
