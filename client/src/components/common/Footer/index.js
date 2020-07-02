556c772214ba59d7974e09c49a74cc6ebecf02fa
AlaaSaadeddin <alaa.swaireh@gmail.com>
2020-07-02 13:11:13 +0300

    remove font awesome library

--- client/src/components/common/Footer/index.js
+++ client/src/components/common/Footer/index.js
@@ -1,18 +1,4 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faEnvelope,
-  faMapMarker,
-  faPhone,
-} from '@fortawesome/free-solid-svg-icons';
-import {
-  faTwitter,
-  faInstagram,
-  faFacebookSquare,
-  faBehance,
-  faSkype,
-  faPinterest,
-} from '@fortawesome/free-brands-svg-icons';
 import logo from '../../../assets/images/berwaz.png';
 import './style.css';
 
@@ -23,45 +9,31 @@ const Footer = () => {
         <div className="follow-container">
           <p>تابعنا على</p>
           <div className="follow-icons">
-            <a
+            {/* <a
               href="https://twitter.com"
               target="_blank"
               rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon icon={faTwitter} color="#636D73" size="2x" />
-            </a>
+            />
             <a
               href="https://pinterest.com"
               target="_blank"
               rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon icon={faPinterest} color="#636D73" size="2x" />
-            </a>
+            />
             <a
               href="https://instagram.com"
               target="_blank"
               rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon icon={faInstagram} color="#636D73" size="2x" />
-            </a>
+            />
             <a
               href="https://www.facebook.com"
               target="_blank"
               rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon
-                icon={faFacebookSquare}
-                color="#636D73"
-                size="2x"
-              />
-            </a>
+            />
             <a
               href="https://www.behance.net"
               target="_blank"
               rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon icon={faBehance} color="#636D73" size="2x" />
-            </a>
+            /> */}
           </div>
         </div>
         <div className="contact-container">
@@ -69,31 +41,26 @@ const Footer = () => {
           <div className="contact-icons">
             <div className="icon-1">
               <p>غزة - فلسطين</p>
-              <FontAwesomeIcon icon={faMapMarker} color="#636D73" size="lg" />
             </div>
             <div className="icon-2">
               <p>+972-000000</p>
-              <FontAwesomeIcon icon={faPhone} color="#636D73" size="lg" />
             </div>
             <div className="icon-3">
               <p>berwaz@gmail.com</p>
-              <a
+              {/* <a
                 href="mailto:berwaz@gmail.com"
                 target="_blank"
                 rel="noopener noreferrer"
-              >
-                <FontAwesomeIcon icon={faEnvelope} color="#636D73" size="lg" />
-              </a>
+              /> */}
             </div>
             <div className="icon-4">
               <p>berwaz</p>
-              <a
+              {/* <a
                 href="https://skype.com"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <FontAwesomeIcon icon={faSkype} color="#636D73" size="lg" />
-              </a>
+              </a> */}
             </div>
           </div>
         </div>
