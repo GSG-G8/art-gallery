d0721f3579f1ca66fb6fe171a6f7f8707c9cfb39
AlaaSaadeddin <alaa.swaireh@gmail.com>
2020-07-05 11:48:41 +0300

    Add Authorization Consumer

--- client/src/components/common/Navbar/index.js
+++ client/src/components/common/Navbar/index.js
@@ -1,31 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { faShoppingCart } from 'react-icons/fa';
+import { FaShoppingCart } from 'react-icons/fa';
 import * as ROUTES from '../../../constants/routes';
+import AuthorizationContext from '../../../Contexts/AuthorizationContext';
+import LogoutContext from '../../../Contexts/LogoutContext';
 import navLogo from '../../../assets/images/berwaz.png';
 import './style.css';
 
 const NavbarComponent = () => {
   return (
     <div className="main-navbar-container">
-      <div className="navbar-container">
-        <div className="nav-left">
-          <Link to={ROUTES.CART_PAGE}>
-            <faShoppingCart className="cart-icon" />
-          </Link>
-          <Link to={ROUTES.SIGNUP_PAGE} className="nav-link">
-            مستخدم جديد
-          </Link>
-          <Link to={ROUTES.LOGIN_PAGE} className="btn btn-primary">
-            تسجيل الدخول
-          </Link>
-        </div>
-        <div className="nav-right">
-          <Link to={ROUTES.HOME_PAGE}>
-            <img className="nav-logo" src={navLogo} alt="logo" />
-          </Link>
-        </div>
-      </div>
+      <AuthorizationContext.Consumer>
+        {({ user }) => (
+          <div className="navbar-container">
+            <div className="nav-left">
+              <Link to={ROUTES.CART_PAGE}>
+                <FaShoppingCart className="cart-icon" />
+              </Link>
+              <Link to={ROUTES.SIGNUP_PAGE} className="nav-link">
+                مستخدم جديد
+              </Link>
+              <Link to={ROUTES.LOGIN_PAGE} className="btn btn-primary">
+                تسجيل الدخول
+              </Link>
+            </div>
+            <div className="nav-right">
+              <Link to={ROUTES.HOME_PAGE}>
+                <img className="nav-logo" src={navLogo} alt="logo" />
+              </Link>
+            </div>
+          </div>
+        )}
+      </AuthorizationContext.Consumer>
     </div>
   );
 };
