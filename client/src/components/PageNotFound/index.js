b6b92678aa9d42f013e1a98e13951af2e9284dfb
AlaaSaadeddin <alaa.swaireh@gmail.com>
2020-07-05 16:43:28 +0300

    edit the style

--- client/src/components/PageNotFound/index.js
+++ client/src/components/PageNotFound/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Button } from 'antd';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
-import notFoundImg from '../../assets/images/notFoundImg.png';
+import notFoundImg from '../../assets/images/notFoundImg.svg';
 import './style.css';
 
 const PageNotFound = () => {
   return (
     <div className="not-found-container">
-      <img src={notFoundImg} alt="notFound" />
+      <img src={notFoundImg} alt="notFound" className="not-found-img" />
       <div>
-        <h1>عذراََ</h1>
+        <h1> ...عذراََ</h1>
         <p>هذه الصفحة غير متوفرة</p>
         <Link to={ROUTES.HOME_PAGE}>
           <Button>الصفحة الرئيسية</Button>
