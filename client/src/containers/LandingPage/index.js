6f76e83b8155abe16b0ea6f20b21a9f2992308ee
Lina Ebeid <linaebe0@gmail.com>
2020-07-06 11:16:46 +0300

    style footer and paintingSection

--- client/src/containers/LandingPage/index.js
+++ client/src/containers/LandingPage/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PaintingsSection from '../../components/PaintingSection';
 import Footer from '../../components/common/Footer';
+import './style.css';
 
 function LandingPage() {
   return (
     <div>
       <PaintingsSection />
-      <Footer />
+      <Footer className="landingP__footer" />
     </div>
   );
 }
